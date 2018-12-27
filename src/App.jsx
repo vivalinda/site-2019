ddf9d374a3acea91a725d4e4548c5f1ae9f9b937
Ronaldo Lima <ron@ldinho.com.br>
2018-12-26 22:07:50 -0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import './App.css'
+
+const App = () => {
+
+  return (
+      <div>
+        <h1>Vivalinda 2019</h1>
+    </div>
+  )
+}
+
+export default  hot(module)(App)
